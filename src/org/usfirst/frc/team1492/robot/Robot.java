0dc6dec2a4abbbabd248be6a1008b60900d6b4c6
nedearb <nedearb@gmail.com>
2015-01-13 18:36:12 -0700

    Update

--- src/org/usfirst/frc/team1492/robot/Robot.java
+++ src/org/usfirst/frc/team1492/robot/Robot.java
@@ -6,27 +6,44 @@ http://wpilib.screenstepslive.com/s/4485/m/13809/l/145002-installing-eclipse-c-j
 package org.usfirst.frc.team1492.robot;
 
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Joystick.ButtonType;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Solenoid;;
 
 
 public class Robot extends SampleRobot {
     RobotDrive drive;
-    Joystick stick;
+    Joystick stickLeft;
+    Joystick stickRight;
+    Solenoid testSolenoid;
+    Compressor compressor;
 
-    public Robot() {//         fl rl fr rl
-    	drive = new RobotDrive(0, 1, 2, 3);
+    public Robot() {//         fl bl fr br
+    	drive = new RobotDrive(1, 3, 0, 2);
     	drive.setExpiration(0.1);
     	
-        stick = new Joystick(0);
+        stickLeft = new Joystick(0);
+        stickRight = new Joystick(1);
+        
+        testSolenoid = new Solenoid(3);
+        
+        compressor = new Compressor();
+        compressor.setClosedLoopControl(true);
+        //compressor.start();
     }
     
     public void autonomous() {
     	drive.setSafetyEnabled(false);
     	
-        //Auto Code
+    	drive.setLeftRightMotorOutputs(-.5, -.5);
+    	Timer.delay(1);
+    	drive.setLeftRightMotorOutputs(0, 0);
+    	
+        
     }
     
     public void operatorControl() {
@@ -34,7 +51,13 @@ public class Robot extends SampleRobot {
     	
         while (isOperatorControl() && isEnabled()) {
         	
-        	//Operation Code
+        	drive.tankDrive(stickRight, stickLeft);
+        	
+        	if(stickRight.getButton(ButtonType.kTrigger)){
+        		testSolenoid.set(true);
+        	}else{
+        		testSolenoid.set(false);
+        	}
         	
             Timer.delay(0.005);
         }
