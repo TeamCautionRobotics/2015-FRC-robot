bbe90f06ac298f8861b4fcfe7520138fbc140a0a
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2015-02-14 13:19:17 -0700

    Initalize autonomous mode hash map

--- src/org/usfirst/frc/team1492/robot/Robot.java
+++ src/org/usfirst/frc/team1492/robot/Robot.java
@@ -55,7 +55,7 @@ public class Robot extends SampleRobot {
 
 	double[] liftPosPresets = { 0, .25, .5, .75, 1 };
 	
-	HashMap<Integer, String> autoModes;
+	HashMap<Integer, String> autoModes = new HashMap<Integer, String>();
 
 	int autoModeNone = 0;
 	int autoModeMoveForward = 1;
